ee64823ace93089a2c51e2f6721f1beaec4d0387
Sebastien LALLEMAND <sld@redhat.com>
2024-02-21 16:07:30 +0100

    update

--- src/dynamic/index.ts
+++ src/dynamic/index.ts
@@ -1,4 +1,5 @@
 import { BackendDynamicPluginInstaller } from '@backstage/backend-dynamic-feature-service';
+import { createRouter } from '@aws/aws-proton-backend-plugin-for-backstage';
 
 export const dynamicPluginInstaller: BackendDynamicPluginInstaller = {
   kind: 'legacy',
@@ -8,33 +9,38 @@ export const dynamicPluginInstaller: BackendDynamicPluginInstaller = {
   // If a contribution is not used, the field should be omitted.
 
   router: {
-    pluginID: 'router plugin ID, used as REST endpoint suffix',
-    createPlugin(env) {
+    pluginID: 'aws-proton',
+    async createPlugin(env) {
       // Return a promise to your router.
-      return Promise.reject(new Error('Not implemented'));
+      //return Promise.reject(new Error('Not implemented'));
+      
+      return await createRouter({
+        logger: env.logger,
+        config: env.config,
+      });
     },
   },
 
-  events(eventsBackend, env) {
-    // Do something with the events backend (add subscribers or publishers)
-    // and return a list of HttpPostIngressOptions that will be
-    // registered with the http event endpoint.
-    return [];
-  },
+  // events(eventsBackend, env) {
+  //   // Do something with the events backend (add subscribers or publishers)
+  //   // and return a list of HttpPostIngressOptions that will be
+  //   // registered with the http event endpoint.
+  //   return [];
+  // },
 
-  catalog(builder, env) {
-    // Add catalog contributions, such as
-    // entity providers or location analyzers.
-  },
+  // catalog(builder, env) {
+  //   // Add catalog contributions, such as
+  //   // entity providers or location analyzers.
+  // },
 
-  scaffolder(env) {
-    // Return an array of scaffolder actions (TemplateAction)
-    // that will be registered with the scaffolder.
-    return [];
-  },
+  // scaffolder(env) {
+  //   // Return an array of scaffolder actions (TemplateAction)
+  //   // that will be registered with the scaffolder.
+  //   return [];
+  // },
 
-  search(indexBuilder, schedule, env) {
-    // Add search contributions, such as
-    // collators and decorators.
-  },
+  // search(indexBuilder, schedule, env) {
+  //   // Add search contributions, such as
+  //   // collators and decorators.
+  // },
 };
